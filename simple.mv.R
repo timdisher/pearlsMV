d5a2e7b71bccdf6534d71f4006643f25f9262047
Timothy Disher <timothydisher@Timothys-Mac-mini.local>
2022-04-10 13:46:21 -0300

    Update data with more iterations for stability

--- simple.mv.R
+++ simple.mv.R
@@ -49,76 +49,8 @@ sim_cor <- purrr::map2(grid$iter,grid$cor, ~ {
   }) %>%
   dplyr::bind_rows()
 
-usethis::use_data(sim_cor)
+usethis::use_data(sim_cor, overwrite = TRUE)
 
 
 
-cond_sim <- sim_cor %>%
-  dplyr::group_by(cor) %>%
-  dplyr::filter(nsig > 0) %>%
-                  dplyr::summarise(nig_ifany = mean(nsig))
-
-
-
-
-data.frame(
-y1 = rnorm(1000) >0,
-y2 = rnorm(1000) >0,
-y3 = rnorm(1000) >0,
-y4 = rnorm(1000) >0) %>%
-  dplyr::mutate(same = y1+y2+y3+y4) %>%
-  dplyr::summarise(mean(same))
-
-y1 %>%
-  dplyr::mutate_all(~ .x > 0) %>%
-  dplyr::mutate(same = V1+V2+V3+V4) %>%
-  dplyr::summarise(mean(same))
-
-
-y2 %>%
-  dplyr::mutate_all(~ .x > 0) %>%
-  dplyr::mutate(same = V1+V2+V3+V4) %>%
-  dplyr::summarise(mean(same))
-
-
-
-sim <- function() purrr::map_dbl(1:1000, ~ {
-
-
-
-})
-
-
-
-purrr::map_dbl(1:100, ~{
-  all <- MASS::mvrnorm(500, rep(0, 4), Sigma = diag(1, nrow = 4)) %>%
-    as.data.frame() %>%
-    dplyr::mutate(trt = rbinom(500, 1, 0.5))
-
-  t <- all %>%
-    tidyr::pivot_longer(cols = -trt) %>%
-    dplyr::group_by(trt, name) %>%
-    dplyr::summarise(mean = mean(value)) %>%
-    dplyr::arrange(name,trt) %>%
-    dplyr::group_by(name) %>%
-    dplyr::summarise_all(~ dplyr::last(.x) - dplyr::first(.x)) %>%
-    dplyr::mutate(pos = mean >= 0) %>%
-    dplyr::summarise(pos = sum(pos)) %>%
-    dplyr::pull()
-
-  t
-}) %>% mean()
-
-
-
-purrr::map_dbl(1:100, ~ {
-  data.frame(y1 = mean(rnorm(500)) - mean(rnorm(500)),
-             y2 = mean(rnorm(500)) - mean(rnorm(500)),
-             y3 = mean(rnorm(500)) - mean(rnorm(500)),
-             y4 = mean(rnorm(500)) - mean(rnorm(500))) %>%
-    dplyr::mutate_all(~ . >= 0) %>%
-    dplyr::mutate(sum = y1 + y2 + y3 + y4) %>%
-    dplyr::pull()
-
-}) %>% mean()
 
